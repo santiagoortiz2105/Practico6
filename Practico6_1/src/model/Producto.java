de0ede619fd84cbc3de446d7450cd96c07cb2d4f
Lucero Roque Roberto Miguel <xeniamancenido@gmail.com>
2025-09-08 20:30:12 -0300

    model

--- Practico6_1/src/model/Producto.java
+++ Practico6_1/src/model/Producto.java
@@ -9,5 +9,45 @@ package model;
  * @author Xenia
  */
 public class Producto {
-    
+    private String nombre;
+    private String categoria;
+    private double precio;
+
+    public Producto() {
+    }
+
+    public Producto(String nombre, String categoria, double precio) {
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " (" + categoria + ") - $" + precio;
+    }
 }
