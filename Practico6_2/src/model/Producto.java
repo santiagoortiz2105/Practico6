a0a5e89537dfb9a0175c0efa28b6b7e04d89b12b
Lucero Roque Roberto Miguel <xeniamancenido@gmail.com>
2025-09-10 21:15:08 -0300

    clase

--- Practico6_2/src/model/Producto.java
+++ Practico6_2/src/model/Producto.java
@@ -8,6 +8,93 @@ package model;
  *
  * @author santi
  */
-public class Producto {
+import java.util.Objects;
+
+public class Producto implements Comparable<Producto> {
+    private int codigo;
+    private String descripcion;
+    private double precio;
+    private int stock;
+    private Rubro rubro;
+
     
+    public Producto(int codigo, String descripcion, double precio, int stock, Rubro rubro) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.stock = stock;
+        this.rubro = rubro;
+    }
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public Rubro getRubro() {
+        return rubro;
+    }
+
+    public void setRubro(Rubro rubro) {
+        this.rubro = rubro;
+    }
+    
+    @Override
+    public int compareTo(Producto otro) {
+        return Integer.compare(this.codigo, otro.codigo);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(codigo);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Producto other = (Producto) obj;
+        return this.codigo == other.codigo;
+    }
+    @Override
+    public String toString() {
+        return "Producto{" + "codigo=" + codigo + ",descripcion='" + descripcion + '\'' + ",precio=" + precio + ",stock=" + stock + ",rubro=" + rubro + '}';
+    }
+    public enum Rubro {
+        COMESTIBLE,
+        LIMPIEZA,
+        PERFUMERIA
+    }
 }
