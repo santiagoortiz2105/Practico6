639ccdf63003b92945b5559ccbbf5d0b30896079
Lourdes <luliortiz222@gmail.com>
2025-09-13 21:38:23 -0300

    Practico6_2

--- Practico6_2/src/app/NewMain.java
+++ Practico6_2/src/app/NewMain.java
@@ -3,11 +3,31 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package app;
-
+import javax.swing.JFrame;
+import javax.swing.JDesktopPane;
+import vistas.frmGestiondeProductos;
 /**
  *
  * @author Lulim
  */
 public class NewMain {
-    
+    public static void main(String[] args) {
+        // Ventana principal
+        JFrame ventana = new JFrame("Gestión de Productos");
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Se crea el escritorio
+        JDesktopPane escritorio = new JDesktopPane();
+        ventana.add(escritorio);
+
+        // se crea la ventana de Gestión de Productos
+        frmGestiondeProductos frmProductos = new frmGestiondeProductos();
+        frmProductos.setVisible(true);
+
+        // Se agrega al escritorio
+        escritorio.add(frmProductos);
+
+        // Se muestra la ventana principal 
+        ventana.setVisible(true);
+    }
 }
