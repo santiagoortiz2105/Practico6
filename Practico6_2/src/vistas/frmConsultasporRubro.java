d174395d5786ef88fd0593bb19e11655bc4cf00c
Lourdes <luliortiz222@gmail.com>
2025-09-16 13:16:45 -0300

    Update frmConsultasporRubro.java

--- Practico6_2/src/vistas/frmConsultasporRubro.java
+++ Practico6_2/src/vistas/frmConsultasporRubro.java
@@ -1,5 +1,5 @@
 package vistas;
-
+//Integrantes: Manceñido Xenia, Ortiz Lourdes, Ortiz Santiago, Ozan Santiago 
 import javax.swing.table.DefaultTableModel;
 import model.CatalogodeProductos;
 import model.Producto;
